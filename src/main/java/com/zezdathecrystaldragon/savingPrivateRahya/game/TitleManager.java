dd8bdcf57e8d7218a69098af96d44c17e4c231e1
dimwit105 <dimwit105@gmail.com>
2026-03-03 06:18:00 -0500

    I realized we can just make TitleManager so much better.

--- src/main/java/com/zezdathecrystaldragon/savingPrivateRahya/game/TitleManager.java
+++ src/main/java/com/zezdathecrystaldragon/savingPrivateRahya/game/TitleManager.java
@@ -10,35 +10,22 @@ import org.bukkit.entity.Player;
 
 import java.time.Duration;
 
-public class TitleManager
-{
+public class TitleManager {
     Title currentlyShownTitle = null;
     Game game;
-    public TitleManager(Game game) {this.game = game;}
 
-    public void sendTitleToOnlineOneSecond(Component what)
-    {
-        Title.Times times = Title.Times.times(Ticks.duration(0), Ticks.duration(20), Ticks.duration(0));
-        Title title = Title.title(what, Component.empty(), times);
-        for (Participant part : game.getParticipants().values())
-        {
-            if(part.getPlayer() == null)
-                continue;
-            showTitleToPlayer(part.getPlayer(), title);
-        }
+    public TitleManager(Game game) {
+        this.game = game;
     }
-    private void showTitleToPlayer(Player p, Title t)
-    {
-        if(currentlyShownTitle == null) {
-            currentlyShownTitle = t;
-            Title.Times times = t.times();
+
+    public void sendTitleToOnlineOneSecond(Component what) {
+        if (currentlyShownTitle == null) {
+            Title.Times times = Title.Times.times(Ticks.duration(0), Ticks.duration(20), Ticks.duration(0));
+            Title title = Title.title(what, Component.empty(), times);
+            currentlyShownTitle = title;
+            SavingPrivateRahya.PLUGIN.getServer().showTitle(title);
             Duration totalTitleLength = times.fadeIn().plus(times.stay().plus(times.fadeOut()));
-            SavingPrivateRahya.PLUGIN.getFoliaLib().getScheduler().runLater(() -> currentlyShownTitle = null, (totalTitleLength.toMillis() / Ticks.SINGLE_TICK_DURATION_MS)-1);
+            SavingPrivateRahya.PLUGIN.getFoliaLib().getScheduler().runLater(() -> currentlyShownTitle = null, (totalTitleLength.toMillis() / Ticks.SINGLE_TICK_DURATION_MS) - 1);
         }
-
-        if(currentlyShownTitle != t)
-            return;
-        p.showTitle(t);
-
     }
 }
